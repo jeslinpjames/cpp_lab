9fb2dd4b0f27633dda9c30eb9ba6778441e29f9a
jeslinpjames <jeslinpjames007@gmail.com>
2023-02-01 16:34:20 +0530

    fimn

--- lab_cycle_1/QUESTION_5/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-cout<<"The sum of the two matrices is: "<<endl;
-    // for (int i=0; i<r; i++)
-    // {
-    //     for (int j=0; j<c; j++)
-    //     {
-    //         cout<<sum.a[i][j]<<" ";
-    //     }
-    //     cout<<endl<<" ";
-    // }
