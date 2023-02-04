95956440659abf7545078e5f61bd7eb9f9bb8493
jeslinpjames <jeslinpjames007@gmail.com>
2023-02-04 18:42:47 +0530

    shop  and objects fin

--- lab_cycle_1/QUESTION_10/shop.cpp
+++ lab_cycle_1/QUESTION_10/shop.cpp
@@ -2,4 +2,24 @@
 using namespace std;
 class shop
 {
-
+    private:
+    int code;
+    string name;
+    float price;
+    int quantity;
+    public:
+    void delete_item();
+    void add_item();
+    void print();
+};
+shop::add_item()
+{
+    cout<<"Enter the Product name : ";
+    cin>>name;
+    cout<<"Enter the Product code : ";
+    cin>>code;
+    cout<<"Enter the price of the Product : ";
+    cin>>price;
+    cout<<"Enter the Quantity : ";
+    cin>>quantity;
+}
