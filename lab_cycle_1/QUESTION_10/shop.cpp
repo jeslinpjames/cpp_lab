2eca698198e059e54c565ca164a350ab1a5cbbde
jeslinpjames <jeslinpjames007@gmail.com>
2023-02-05 18:05:24 +0530

    shop darft

--- lab_cycle_1/QUESTION_10/shop.cpp
+++ lab_cycle_1/QUESTION_10/shop.cpp
@@ -10,9 +10,22 @@ class shop
     public:
     void delete_item();
     void add_item();
-    void print();
+    void display_item();
+    int return_code()
+    {
+          return code;
+    }
+    shop()
+    {}
+    shop(const shop &i)
+    {
+        code=i.code;
+        name=i.name;
+        price=i.price;
+        quantity=i.quantity;
+    }
 };
-shop::add_item()
+void shop::add_item()
 {
     cout<<"Enter the Product name : ";
     cin>>name;
@@ -23,9 +36,67 @@ shop::add_item()
     cout<<"Enter the Quantity : ";
     cin>>quantity;
 }
-void shop::print()
+void shop::display_item()
 {
-
+    cout<<name<<code<<price<<quantity<<endl;   
 }
-void delete_item();
-
+int main()
+{
+    cout<<"-----------------SHOP-------------------"<<endl;
+    int choice=0;
+    shop ob[100];
+    int i=1;
+    do
+    {
+        cout<<"Enter 1 to add an item to the shopping list."<<endl;
+        cout<<"Enter 2 to remove an item from the shopping list."<<endl;
+        cout<<"Enter 3 to print the total value of the purchase order."<<endl;
+        cout<<"Enter 4 to exit.";
+        cin>>choice;
+        if(choice==1)
+        {
+            cout<<"---------------ADD ITEM---------------"<<endl;
+            ob[i].add_item();
+            i++;
+        }
+        else if(choice ==2)
+        {
+            int cod,j=0;
+            cout<<"Enter the product code of the item to be deleted:";
+            cin>>cod;
+            for(j=1;i<=i;j++)
+            {
+                int code1=ob[j].return_code();
+                if(code1==cod)
+                {
+                    break;
+                }                
+            }
+            for(int k=j;k<i;k++)
+            {
+                ob[k]=ob[k+1];
+            }
+            i--;
+        }
+        else if(choice ==3)
+        {
+            cout<<"---------------RECEIPT---------------"<<endl;
+            for(int j=1;j<=i;j++)
+            {
+                ob[j].display_item();
+            }
+            cout<<"--------------------------------------"<<endl;
+        }
+        else if(choice==4)
+        {
+            cout<<"Thank You"<<endl;
+            return 0;
+        }
+        else
+        {
+            cout<<"Wrong Choice. renter"<<endl;
+            cin>>choice;            
+        }
+    } while ((choice>=1)&&(choice<=3));
+    return 0;   
+}
