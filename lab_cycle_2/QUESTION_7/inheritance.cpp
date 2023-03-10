c5ee33e0435f7c878be484c873fbfc4d0d63560f
jeslinpjames <jeslinpjames007@gmail.com>
2023-03-10 22:31:10 +0530

    beta added

--- lab_cycle_2/QUESTION_7/inheritance.cpp
+++ lab_cycle_2/QUESTION_7/inheritance.cpp
@@ -3,16 +3,30 @@ using namespace std;
 class alpha
 {
     private:
-    int x;
+         x;
     public:
-    aplha(int i)   
-    {
-	    x=i;
-	    cout<<"Alpha has been Constructed"<<endl;
-    }
-    void showx()
-    {
-	    cout<<"The value of x is : "<<x<<endl;
-    }
+        aplha(int i)   
+        {
+            x=i;
+            cout<<"Alpha has been Constructed"<<endl;
+        }
+        void showx()
+        {
+            cout<<"The value of x is : "<<x<<endl;
+        }
 };
-
+class beta
+{
+	private:
+		float y;
+	public:
+		beta(float j)
+		{
+			y=j;
+			cout<<"Beta has been Constructed "<<endl;
+		}
+		void showy()
+		{
+			cout<<"The value of y is : "<<y<<endl;
+		}
+};
