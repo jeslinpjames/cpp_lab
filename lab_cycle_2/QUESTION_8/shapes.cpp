2c6330d5c5ce5fccb33ca3693b1c897252b46100
jeslinpjames <jeslinpjames007@gmail.com>
2023-03-11 12:30:05 +0530

    Ellipse added

--- lab_cycle_2/QUESTION_8/shapes.cpp
+++ lab_cycle_2/QUESTION_8/shapes.cpp
@@ -5,7 +5,7 @@ class Shapes
     protected:
         float area;
     public:
-        virtual void area();
+        virtual void find_area();
         virtual void input();
 };
 class Circle:public Shapes
@@ -18,7 +18,7 @@ class Circle:public Shapes
             cout<<"Enter the raius of the Circle : ";
             cin>>radius;
         }
-        void area()
+        void find_area()
         {
             area = (22.0/7.0)*radius*radius;
             cout<<"The area of the Circle is : "<<area<<endl;             
@@ -34,10 +34,28 @@ class Square:public Shapes
             cout<<"Enter the side of the Square : ";
             cin>>side;
         }
-        void area()
+        void find_area()
         {
             area= side*side;
             cout<<"The area of the Square is : "<<area<<endl;
+        }
+};
+class Ellipse:public Shapes
+{
+    private:
+        float major_axis,minor_axis;
+    public:
+        void input()
+        {
+            cout<<"Enter the value of the Major Axis of the Ellipse : "
+            cin>>major_axis;
+            cout<<"Enter the value of the Minor Axis of the Ellipse : "
+            cin>>minor_axis;
+        }
+        void find_area()
+        {
+            area = (22.0/7.0)*major_axis*minor_axis;
+            cout<<"The area of the Ellipse is : "<<area<<endl;
         }
 };
 
