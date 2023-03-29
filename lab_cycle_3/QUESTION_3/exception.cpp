c59706462134e0d3fd281abb4fa40361fce4e6c3
jeslinpjames <jeslinpjames007@gmail.com>
2023-03-29 14:05:23 +0530

    conf

--- lab_cycle_3/QUESTION_3/exception.cpp
+++ lab_cycle_3/QUESTION_3/exception.cpp
@@ -9,22 +9,13 @@ class exep
     {}
     double divide()
     {
-<<<<<<< HEAD
-        double q=0.0;
-=======
         double q=0;
->>>>>>> a3dcdacdde477df4d9900fc1e4244a352a23108f
         try
         {
             if(b==0)
                 throw runtime_error("Divison by zero not possible!");
-<<<<<<< HEAD
             else
                q=a/b;                 
-=======
-            else 
-                q=(a/b);
->>>>>>> a3dcdacdde477df4d9900fc1e4244a352a23108f
         }
         catch(exception &e)
         {
@@ -32,13 +23,6 @@ class exep
         }
         return q;
     }
-<<<<<<< HEAD
-    void input()
-    {
-        
-    }
-=======
->>>>>>> a3dcdacdde477df4d9900fc1e4244a352a23108f
 };
 int main()
 {
@@ -48,7 +32,6 @@ int main()
     while(choice1==1)
     {
         int n1,n2;
-<<<<<<< HEAD
         try
         {
             cout<<"Enter the first number : ";
@@ -69,15 +52,6 @@ int main()
         }
         exep ob(n1,n2);
         double q= ob.divide();
-=======
-        cout<<"Enter the first number : ";
-        cin>>n1;
-        cout<<"Enter the second number : ";
-        cin>>n2;
-        exep ob(n1,n2);
-        double q= ob.divide();
-        if(q!=0&&n2!=0)
->>>>>>> a3dcdacdde477df4d9900fc1e4244a352a23108f
         cout<<n1<<" / "<<n2<<" = "<<q<<endl;
         cout<<"Enter 1 to Continue,\nEnter 2 to Exit : ";
         cin>>choice1;
