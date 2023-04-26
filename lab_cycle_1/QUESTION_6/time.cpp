e5aec608f24a4946d6ef1f68350ecdbfd187d498
jeslinpjames <jeslinpjames007@gmail.com>
2023-04-26 19:54:57 +0530

    q 5 6 7 beautified

--- lab_cycle_1/QUESTION_6/time.cpp
+++ lab_cycle_1/QUESTION_6/time.cpp
@@ -1,52 +1,48 @@
 #include<iostream>
+
 using namespace std;
-class Time
-{
-    private:
-    int seconds,minutes,hour;
-    public:
-    Time(){
-        seconds=0;
-        minutes=0;
-        hour =0;
-    }
-    void accept();
-    void display();
-    void sum(Time,Time);
+class Time {
+  private: int seconds,
+  minutes,
+  hour;
+  public: Time() {
+    seconds = 0;
+    minutes = 0;
+    hour = 0;
+  }
+  void accept();
+  void display();
+  void sum(Time, Time);
 };
-int main()
-{
-    Time t1,t2,t3;
-    cout<<"Enter the frist time : ";
-    t1.accept();
-    cout<<"Enter the second time : ";
-    t2.accept();
-    t3.sum(t1,t2);
-    cout<<"The sum of both the time is : "<<endl;
-    t3.display();
-    return 0;
+int main() {
+  Time t1, t2, t3;
+  cout << "Enter the frist time details : ";
+  t1.accept();
+  cout << "Enter the second time details : ";
+  t2.accept();
+  t3.sum(t1, t2);
+  cout << "The sum of both the time is : " << endl;
+  t3.display();
+  return 0;
 }
-void Time::accept()
-{
-    cout<<"\n Enter time. ";
-    cout<<"\n Hours : ";
-    cin>>hour;
-    cout<<"Minutes : ";
-    cin>>minutes;
-    cout<<"Seconds : ";
-    cin>>seconds;
+void Time::accept() {
+  cout << "\n Enter time. ";
+  cout << "\n Hours : ";
+  cin >> hour;
+  cout << "Minutes : ";
+  cin >> minutes;
+  cout << "Seconds : ";
+  cin >> seconds;
 }
-void Time::sum(Time a,Time b)
-{
-    seconds=a.seconds+b.seconds;
-    minutes=seconds/60;
-    seconds=seconds%60;
-    minutes=minutes+a.minutes+b.minutes;
-    hour= minutes/60;
-    minutes = minutes%60;
-    hour= hour+a.hour+b.hour;
+void Time::sum(Time a, Time b) {
+  seconds = a.seconds + b.seconds;
+  minutes = seconds / 60;
+  seconds = seconds % 60;
+  minutes = minutes + a.minutes + b.minutes;
+  hour = minutes / 60;
+  minutes = minutes % 60;
+  hour = hour + a.hour + b.hour;
 }
-void Time::display()
-{
-    cout<<hour<<" hours "<<minutes<<" minutes "<<seconds<<" seconds."<<endl;
+void Time::display() {
+  cout << hour << " hours " << minutes << " minutes " << seconds << " seconds." << endl;
 }
--- lab_cycle_1/QUESTION_7/mat.cpp
+++ lab_cycle_1/QUESTION_7/mat.cpp
@@ -1,148 +1,119 @@
 #include<iostream>
+
 using namespace std;
-class Mat
-{
-    private:
-    int **a;
-    int r,c;
-    public:
-    Mat(){}
-    Mat(int x,int y)
-    {
-        r = x;
-        c = y;
-        if((r>0)&&(c>0))
-        {
-            a = new int *[r];
-            for (int i=0; i<r; i++)
-            {
-                a[i]= new int [c];
-            }
-        }
+class Mat {
+  private: int ** a;
+  int r,
+  c;
+  public: Mat() {}
+  Mat(int x, int y) {
+    r = x;
+    c = y;
+    if ((r > 0) && (c > 0)) {
+      a = new int * [r];
+      for (int i = 0; i < r; i++) {
+        a[i] = new int[c];
+      }
     }
-    ~Mat()
-    {
-        for (int i=0; i<r; i++)
-            delete a[i];
-        delete a; 
-    }
-    void matrix_add(Mat &x, Mat &y);
-    void matrix_transpose(Mat &x);
-    void matrix_mult(Mat &x, Mat &y);
-    void accept_elements(void);
-    int matrix_trace(void);
-    void display();
+  }
+  ~Mat() {
+    for (int i = 0; i < r; i++)
+      delete a[i];
+    delete a;
+  }
+  void matrix_add(Mat & x, Mat & y);
+  void matrix_transpose(Mat & x);
+  void matrix_mult(Mat & x, Mat & y);
+  void accept_elements(void);
+  int matrix_trace(void);
+  void display();
 };
-void Mat::matrix_add(Mat &x, Mat &y)
-{ 
-   for (int i=0; i<r; i++)
-    {
-        for (int j=0; j<c; j++)
-        {
-            a[i][j]+=x.a[i][j]+y.a[i][j];
-        }
+void Mat::matrix_add(Mat & x, Mat & y) {
+  for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+      a[i][j] += x.a[i][j] + y.a[i][j];
     }
+  }
 }
-void Mat::matrix_transpose(Mat &x)
-{ 
-   for (int i=0; i<r; i++)
-    {
-        for (int j=0; j<c; j++)
-        {
-            a[i][j]=x.a[j][i];
-        }
-        cout<<endl;
+void Mat::matrix_transpose(Mat & x) {
+  for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+      a[i][j] = x.a[j][i];
     }
-    display();
+    cout << endl;
+  }
+  display();
 }
-void Mat::matrix_mult(Mat &x, Mat &y)
-{ 
-   for (int i=0; i<x.r; i++)
-    {
-        for (int j=0; j<y.r; j++)
-        {
-            a[i][j]=0;
-            for(int k=0;k<x.c;k++)
-            {
-                a[i][j]+=x.a[i][k]*y.a[k][j];
-            }
-        }
+void Mat::matrix_mult(Mat & x, Mat & y) {
+  for (int i = 0; i < x.r; i++) {
+    for (int j = 0; j < y.r; j++) {
+      a[i][j] = 0;
+      for (int k = 0; k < x.c; k++) {
+        a[i][j] += x.a[i][k] * y.a[k][j];
+      }
     }
+  }
 }
-int Mat::matrix_trace(void){
-    int sum =0;
-    for(int i=0;i<r;i++)
-    {
-        sum=sum+a[i][i];
-    }    
-    return sum;
+int Mat::matrix_trace(void) {
+  int sum = 0;
+  for (int i = 0; i < r; i++) {
+    sum = sum + a[i][i];
+  }
+  return sum;
 }
-void Mat::accept_elements()
-{
-    for (int i=0; i<r; i++)
-    {
-        cout<<"Enter "<<c<<" elements of "<<i+1<<" row : ";
-        for (int j=0; j<c; j++)
-        {
-            cin>>a[i][j];
-        }
+void Mat::accept_elements() {
+  for (int i = 0; i < r; i++) {
+    cout << "Enter " << c << " elements of " << i + 1 << " row : ";
+    for (int j = 0; j < c; j++) {
+      cin >> a[i][j];
     }
+  }
 }
-void Mat::display()
-{
- for (int i=0; i<r; i++)
- {
-    for (int j=0; j<c; j++)
-    {
-        cout<<a[i][j]<<" ";
+void Mat::display() {
+  for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+      cout << a[i][j] << " ";
     }
-    cout<<endl;
- }
+    cout << endl;
+  }
 }
-int main()
-{
-    int r1,c1,r2,c2;
-    cout<<"Enter the number of rows and columns of the first matrix : "<<endl;
-    cin>>r1>>c1;
-    Mat ob1(r1,c1);
-    cout<<"Enter the values of the first matrix: "<<endl;
-    ob1.accept_elements();
-    cout<<"Enter the number of rows and colums of the second matrix : "<<endl;
-    cin>>r2>>c2;
-    Mat ob2(r2,c2);
-    cout<<"Enter the values of the second matrix: "<<endl;
-    ob2.accept_elements();
-    cout<<"The Transpose of the First Matrix : "<<endl;
-    Mat obj1(c1,r1);
-    obj1.matrix_transpose(ob1);
-    cout<<"\nThe Transpose of the Second Matrix : "<<endl;
-    Mat obj2(c2,r2);
-    obj2.matrix_transpose(ob2);
-    int trace1=ob1.matrix_trace();
-    cout<<"The Trace of the First Matrix : "<<trace1<<endl;
-    int trace2=ob2.matrix_trace();
-    cout<<"\nThe Trace of the Second Matrix : "<<trace2<<endl;
-     if (r1 != r2 || c1 != c2)
-    {
-        cout<<"Matrices must have the same number of rows and columns."<<endl;
-    }
-    else
-    {
-        cout<<"\nThe sum of the two matrices is: "<<endl; 
-        Mat ob3(r1,c1);
-        ob3.matrix_add(ob1, ob2);
-        ob3.display();
-    }
-    if(c1==r2)
-    {
-        Mat ob4(r1,c2);
-        ob4.matrix_mult(ob1,ob2);
-        cout<<"\nThe Product of the two matrices is : "<<endl;
-        ob4.display();
-    }
-    else
-    {
-        cout<<"Multiplication not possible because order is not matching."<<endl;
-    }
-    return 0;
+int main() {
+  int r1, c1, r2, c2;
+  cout << "Enter the number of rows and columns of the first matrix : " << endl;
+  cin >> r1 >> c1;
+  Mat ob1(r1, c1);
+  cout << "Enter the values of the first matrix: " << endl;
+  ob1.accept_elements();
+  cout << "Enter the number of rows and colums of the second matrix : " << endl;
+  cin >> r2 >> c2;
+  Mat ob2(r2, c2);
+  cout << "Enter the values of the second matrix: " << endl;
+  ob2.accept_elements();
+  cout << "The Transpose of the First Matrix : ";
+  Mat obj1(c1, r1);
+  obj1.matrix_transpose(ob1);
+  cout << "\nThe Transpose of the Second Matrix : ";
+  Mat obj2(c2, r2);
+  obj2.matrix_transpose(ob2);
+  int trace1 = ob1.matrix_trace();
+  cout << "The Trace of the First Matrix : " << trace1 << endl;
+  int trace2 = ob2.matrix_trace();
+  cout << "\nThe Trace of the Second Matrix : " << trace2 << endl;
+  if (r1 != r2 || c1 != c2) {
+    cout << "Matrices must have the same number of rows and columns." << endl;
+  } else {
+    cout << "\nThe sum of the two matrices is: " << endl;
+    Mat ob3(r1, c1);
+    ob3.matrix_add(ob1, ob2);
+    ob3.display();
+  }
+  if (c1 == r2) {
+    Mat ob4(r1, c2);
+    ob4.matrix_mult(ob1, ob2);
+    cout << "\nThe Product of the two matrices is : " << endl;
+    ob4.display();
+  } else {
+    cout << "Multiplication not possible because order is not matching." << endl;
+  }
+  return 0;
 }
