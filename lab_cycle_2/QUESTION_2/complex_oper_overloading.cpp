d557a1cb1f4cd84ded20c46f9cb3d4335013b847
jeslinpjames <jeslinpjames007@gmail.com>
2023-04-27 16:02:37 +0530

    q3 q2 c2 updated

--- lab_cycle_2/QUESTION_2/complex_oper_overloading.cpp
+++ lab_cycle_2/QUESTION_2/complex_oper_overloading.cpp
@@ -1,117 +1,97 @@
 #include<iostream>
+
 using namespace std;
-class complex
-{
-	private:
-		float real,imag;
-	public:
-		void input()
-		{
-			cout<<"Enter the real part of the complex number : ";
-			cin>>real;
-			try
-			{
-				if(cin.fail())
-				{
-					throw runtime_error("Invalid Input!");
-				}				
-			}
-			catch(runtime_error&e)
-			{
-				cerr<<e.what()<<endl;
-			}
-			cout<<"Enter the imaginary part of the complex number : ";
-			cin>>imag;
-			try
-			{
-				if(cin.fail())
-				{
-					throw runtime_error("Invalid Input!");
-				}
-			}
-			catch(runtime_error & e)
-			{
-				cerr<<e.what()<<endl;
-			}
-		}
-		void display()
-		{
-			cout<<real;
-			if(imag<0)
-			{
-				cout<<imag<<"i"<<endl;
-			}
-			else
-				cout<<"+"<<imag<<"i"<<endl;
-		}
-		friend complex operator+(complex a, complex b);
-		friend complex operator*(complex a, complex b);
+class complex {
+  private: float real,
+  imag;
+  public: void input() {
+    cout << "Enter the real part : ";
+    cin >> real;
+    try {
+      if (cin.fail()) {
+        throw runtime_error("Invalid Input!");
+      }
+    } catch (runtime_error & e) {
+      cerr << e.what() << endl;
+    }
+    cout << "Enter the imaginary : ";
+    cin >> imag;
+    try {
+      if (cin.fail()) {
+        throw runtime_error("Invalid Input!");
+      }
+    } catch (runtime_error & e) {
+      cerr << e.what() << endl;
+    }
+  }
+  void display() {
+    cout << real;
+    if (imag < 0) {
+      cout << imag << "i" << endl;
+    } else
+      cout << "+" << imag << "i" << endl;
+  }
+  friend complex operator + (complex a, complex b);
+  friend complex operator * (complex a, complex b);
 };
-complex operator+(complex a ,complex b)
-{
-	complex c;
-	c.real=a.real+b.real;
-	c.imag=a.imag+b.imag;
-	return c;
+complex operator + (complex a, complex b) {
+  complex c;
+  c.real = a.real + b.real;
+  c.imag = a.imag + b.imag;
+  return c;
 }
-complex operator*(complex a,complex b)
-{
-	complex c;
-	c.real=(a.real*b.real)-(a.imag*b.imag);
-	c.imag=(a.real*b.imag)+(b.real*a.imag);
-	return c;
+complex operator * (complex a, complex b) {
+  complex c;
+  c.real = (a.real * b.real) - (a.imag * b.imag);
+  c.imag = (a.real * b.imag) + (b.real * a.imag);
+  return c;
 }
-int main()
-{
-	int choice;
-	complex a,b,c,d;
-	do
-	{
-		a.input();
-		b.input();
-		cout<<"The first complex number is : ";
-		a.display();
-		cout<<"The second complex number is :";
-		b.display();
-		int choice2=1;
-		while(choice2 ==1)
-		{
-			cout<<"Enter 1 to find the sum of the two complex numbers. "<<endl;
-			cout<<"Enter 2 to find the product of the two complex numbers."<<endl;
-			cout<<"Enter your choice :";
-			int choice1;
-			cin>>choice1;
-			switch(choice1)
-			{
-				case 1:
-					c=a+b;
-					cout<<"The sum of the two complex number is : ";
-					c.display();
-					break;
-				case 2:
-					d=a*b;
-					cout<<"The product of the two complex numbers is : ";
-					d.display();
-					break;
-				default:
-					cout<<"Wrong Choice!"<<endl;
-			}
-			cout<<endl<<endl;
-			cout<<"Enter 1 to continue operations "<<endl;
-			cout<<"Enter 2 or any other number to exit"<<endl;
-			cin>>choice2;
-			cout<<endl;
-		}
-		
-		cout<<endl<<endl;
-		cout<<"To continue and enter new complex number enter 1."<<endl;
-		cout<<"To exit enter any number."<<endl;
-		cout<<"Enter your choice : ";
-		cin>>choice;
-	}while(choice==1);
-	cout<<"Thank You!"<<endl;
-	return 0;
+int main() {
+  int choice;
+  complex a, b, c, d;
+  do {
+    cout << "For the first Complex Number :\n";
+    a.input();
+    cout << "For the second Complex Number :\n";
+    b.input();
+    cout << "The first complex number is : ";
+    a.display();
+    cout << "The second complex number is :";
+    b.display();
+    int choice2 = 1;
+    while (choice2 == 1) {
+      cout << "Enter 1 to find the sum of the two complex numbers. " << endl;
+      cout << "Enter 2 to find the product of the two complex numbers." << endl;
+      cout << "Enter your choice :";
+      int choice1;
+      cin >> choice1;
+      switch (choice1) {
+      case 1:
+        c = a + b;
+        cout << "The sum of the two complex number is : ";
+        c.display();
+        break;
+      case 2:
+        d = a * b;
+        cout << "The product of the two complex numbers is : ";
+        d.display();
+        break;
+      default:
+        cout << "Wrong Choice!" << endl;
+      }
+      cout << endl << endl;
+      cout << "Enter 1 to continue operations " << endl;
+      cout << "Enter 2 to exit or to enter another complex number" << endl;
+      cin >> choice2;
+      cout << endl;
+    }
+
+    cout << endl << endl;
+    cout << "To continue and enter new complex number enter 1." << endl;
+    cout << "To exit enter any number." << endl;
+    cout << "Enter your choice : ";
+    cin >> choice;
+  } while (choice == 1);
+  cout << "Thank You!" << endl;
+  return 0;
 }
-
-
-
--- lab_cycle_2/QUESTION_3/vector_1.cpp
+++ lab_cycle_2/QUESTION_3/vector_1.cpp
@@ -1,79 +1,70 @@
 #include<iostream>
+
 using namespace std;
-class Vector
-{
-	private:
-		float i_comp,j_comp,k_comp;
-	public:
-		friend float operator*(Vector &a,Vector &b);
-		friend istream& operator>>(istream &in, Vector &a);
-		friend ostream& operator<<(ostream &out,Vector &a);
+class Vector {
+  private: float i_comp,
+  j_comp,
+  k_comp;
+  public: friend float operator * (Vector & a, Vector & b);
+  friend istream & operator >> (istream & in, Vector & a);
+  friend ostream & operator << (ostream & out, Vector & a);
 };
-float operator*(Vector& a,Vector& b)
-{
-	return((b.i_comp*a.i_comp)+(b.j_comp*a.j_comp)+(b.k_comp*a.k_comp));
+float operator * (Vector & a, Vector & b) {
+  return ((b.i_comp * a.i_comp) + (b.j_comp * a.j_comp) + (b.k_comp * a.k_comp));
 }
-istream& operator>>(istream &in, Vector& a)
-{
-	cout<<"Enter the component of i : ";
-	in>>a.i_comp;
-	cout<<"Enter the component of j : ";
-	in>>a.j_comp;
-	cout<<"Enter the component of k : ";
-	in>>a.k_comp;
-	return in;
+istream & operator >> (istream & in, Vector & a) {
+  cout << "Enter the component of i : ";
+  in >> a.i_comp;
+  cout << "Enter the component of j : ";
+  in >> a.j_comp;
+  cout << "Enter the component of k : ";
+  in >> a.k_comp;
+  return in;
 }
-ostream& operator<<(ostream &out , Vector& a)
-{
-	if((a.j_comp>=0)&&(a.k_comp>=0))
-		out<<a.i_comp<<"i + "<<a.j_comp<<"j + "<<a.k_comp<<"k "<<endl;
-        else
-        {
-		if((a.j_comp<0)&&(a.k_comp<0))
-			out<<a.i_comp<<"i "<<a.j_comp<<"j "<<a.k_comp<<"k "<<endl;
-               	else if(a.j_comp<0)
-			out<<a.i_comp<<"i "<<a.j_comp<<"j + "<<a.k_comp<<"k "<<endl;
-                else
-			out<<a.i_comp<<"i + "<<a.j_comp<<"j "<<a.k_comp<<"k "<<endl;
-        }
-	return out;
+ostream & operator << (ostream & out, Vector & a) {
+  if ((a.j_comp >= 0) && (a.k_comp >= 0))
+    out << a.i_comp << "i + " << a.j_comp << "j + " << a.k_comp << "k " << endl;
+  else {
+    if ((a.j_comp < 0) && (a.k_comp < 0))
+      out << a.i_comp << "i " << a.j_comp << "j " << a.k_comp << "k " << endl;
+    else if (a.j_comp < 0)
+      out << a.i_comp << "i " << a.j_comp << "j + " << a.k_comp << "k " << endl;
+    else
+      out << a.i_comp << "i + " << a.j_comp << "j " << a.k_comp << "k " << endl;
+  }
+  return out;
 }
-int main()
-{
-	int choice1;
-	do
-	{
-		cout<<"Enter 1 to start,"<<endl;
-		cout<<"Enter 2 or any other number to quit. "<<endl;
-		cout<<"Enter your choice :";
-		cin>>choice1;
-		if(choice1!=1)
-		{
-			break;
-		}
-		cout<<"Enter values of Vector 1 :"<<endl;
-		Vector v1;
-		cin>>v1;
-		cout<<"Enter values of Vector 2 :"<<endl;
-		Vector v2;
-		cin>>v2;
-		cout<<"Vector 1 is : "<<v1<<endl;
-		cout<<"Vector 2 is : "<<v2<<endl;
-		int choice2=0;
-		cout<<"Enter 1 to find the dot product of these 2 vectors."<<endl;
-		cout<<"Enter 2 or any other number to exit."<<endl;
-		cout<<"Enter your choice : ";
-		cin>>choice2;
-		if(choice2==1)
-		{
-			float product;
-			product=v1*v2;
-			cout<<"The dot product is : "<<product<<endl;
-		}
-		else
-		{
-			break;
-		}
-	}while(choice1==1);
-	return 0;
+int main() {
+  int choice1;
+  do {
+    cout << "Enter 1 to start," << endl;
+    cout << "Enter 2 or any other number to quit. " << endl;
+    cout << "Enter your choice :";
+    cin >> choice1;
+    if (choice1 != 1) {
+      break;
+    }
+    cout << "Enter values of Vector 1 :" << endl;
+    Vector v1;
+    cin >> v1;
+    cout << "Enter values of Vector 2 :" << endl;
+    Vector v2;
+    cin >> v2;
+    cout << "\nVector 1 is : " << v1 << endl;
+    cout << "Vector 2 is : " << v2 << endl;
+    int choice2 = 0;
+    cout << "Enter 1 to find the dot product of these 2 vectors." << endl;
+    cout << "Enter 2 or any other number to exit." << endl;
+    cout << "Enter your choice : ";
+    cin >> choice2;
+    if (choice2 == 1) {
+      float product;
+      product = v1 * v2;
+      cout << "The dot product is : " << product << endl;
+    } else {
+      break;
+    }
+  } while (choice1 == 1);
+  cout << "Exiting..." << endl;
+  return 0;
 }
